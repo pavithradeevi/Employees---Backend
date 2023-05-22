4e23f14b72b2932bead29d78202c2c5ed8f7f2c9
pavithra kamalakkannan <pavithradeevi@gmail.com>
2023-05-22 15:03:04 +0530

    changwe

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,12 +8,12 @@ const socketIO = require('socket.io');
 const app = express();
 const port = 3001;
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// MongoDB connection
+
 mongoose.connect('mongodb+srv://employees:employees@cluster0.rburcth.mongodb.net/dashboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -27,21 +26,21 @@ const employeeSchema = new mongoose.Schema({
 
 const Employee = mongoose.model('Employee', employeeSchema);
 
-// Socket.io setup
+
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
 const io = socketIO(server);
 
-// Routes
+
 app.get('/employees', async (req, res) => {
   try {
     const employees = await Employee.find();
     res.json(employees);
   } catch (error) {
     console.error('Error fetching employees:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Error' });
   }
 });
 
@@ -52,10 +51,10 @@ app.post('/employees', async (req, res) => {
     const newEmployee = new Employee({ name, salary });
     await newEmployee.save();
 
-    res.status(201).json({ message: 'Employee added successfully' });
+    res.status(201).json({ message: 'Space Added' });
   } catch (error) {
-    console.error('Error adding employee:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Error' });
   }
 });
 
@@ -72,6 +71,22 @@ app.put('/employees/:id', async (req, res) => {
     res.status(200).json({ message: 'Employee updated successfully' });
   } catch (error) {
     console.error('Error updating employee:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.delete('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Employee.findByIdAndDelete(id);
+
+    
+    io.emit('updateEmployees');
+
+    res.status(200).json({ message: 'Employee deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting employee:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
